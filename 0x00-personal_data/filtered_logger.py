4e0ecbb085893652d293cc50c7685b1239692034
OteIan <oteian9@gmail.com>
2024-04-13 20:15:18 +0300

    Regexing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Filtered logger module
+"""
+from typing import List
+import logging
+import re
+
+
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str
+        ) -> str:
+    """
+    Returns the log message obfuscated
+    """
+    pattern = f"({'|'.join(fields)})=[^{separator}]+"
+
+    return re.sub(pattern,
+                  lambda match: f"{match.group().split('=')[0]}={redaction}",
+                  message)
