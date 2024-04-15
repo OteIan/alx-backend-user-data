df9b2a6c2ffb252fc2a2e0cf96326f0a5c3e151f
OteIan <oteian9@gmail.com>
2024-04-15 22:13:41 +0300

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -17,7 +17,7 @@ class BasicAuth(Auth):
             authorization_header: str
             ) -> str:
         """
-        extract_base64_authorization_header
+        extract base64 authorization header
         """
         if (authorization_header is None or
                 not isinstance(authorization_header, str)):
@@ -25,3 +25,22 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+            ) -> str:
+        """
+        decode base64 authorization header
+        """
+        import base64
+
+        if (base64_authorization_header is None or
+                not isinstance(base64_authorization_header, str)):
+            return None
+        try:
+            return base64.b64decode(
+                base64_authorization_header.encode('utf-8')
+                ).decode('utf-8')
+        except Exception:
+            return None
