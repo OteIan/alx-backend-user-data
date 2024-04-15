2929c9481296214fcebda1419ba9ea272067a3e9
OteIan <oteian9@gmail.com>
2024-04-15 21:08:02 +0300

    Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> str:
       - "Error": "Unauthorized"
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET api/v1/forbidden
+    Return:
+      - "Error": "Forbidden"
+    """
+    abort(403)
