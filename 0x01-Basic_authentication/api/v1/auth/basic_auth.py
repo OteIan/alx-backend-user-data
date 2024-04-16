4b1b3c651b2ccd87971ac67511c1c0dd43d4bf02
Rich_ar_Dev <moriasrichar3@gmail.com>
2024-04-16 20:31:46 +0300

    Unfinished

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,8 @@
 Basic Auth module
 """
 from api.v1.auth.auth import Auth
-from typing import Tuple
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -59,3 +60,16 @@ class BasicAuth(Auth):
         if ":" not in decoded_base64_authorization_header:
             return (None, None)
         return tuple(decoded_base64_authorization_header.split(":"))
+    
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):  # type: ignore
+        """
+        user object from credentials
+        """
+        if user_email is None or user_pwd is None:
+            return None
+        if not isinstance(user_email, str) or not isinstance(user_pwd, str):
+            return 
+        
+        if User.search({user_email}):
+            
+        
