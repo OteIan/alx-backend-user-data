ac9f70c53095e5d35e136486951158e58ef59896
OteIan <oteian9@gmail.com>
2024-04-17 10:38:42 +0300

    Task 2: Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,11 +3,24 @@
 Session Auth Module
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     Session Auth class
     """
+    user_id_by_session_id = {}
+
     def __init__(self) -> None:
         super().__init__()
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        create session
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
