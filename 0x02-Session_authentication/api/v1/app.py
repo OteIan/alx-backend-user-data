f07fdbf3b889528ddd83c4c207e5c1a4a31c6b7d
OteIan <oteian9@gmail.com>
2024-04-19 00:24:19 +0300

    Fix: Return value

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -59,7 +59,7 @@ def before_request() -> None:
                                         '/api/v1/unauthorized/',
                                         '/api/v1/forbidden/',
                                         '/api/v1/auth_session/login/']):
-        if (not auth.authorization_header(request) and
+        if (auth.authorization_header(request) is None and
                 auth.session_cookie(request) is None):
             abort(401)
         if not auth.current_user(request):
